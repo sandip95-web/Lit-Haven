90571efd1342291990b7231866d80e4b34895eb8
sandip95-web <sandipmaharjan95@gmail.com>
2024-06-23 09:14:43 +0545

    quanity added in redux

--- components/BookCart.tsx
+++ components/BookCart.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Image from "next/image";
 import { useAppSelector, useAppDispatch } from "@/lib/supabase/hooks/redux";
-import { getCart } from "@/redux/cartSlice";
+import { getCart, removeFromCart } from "@/redux/cartSlice";
+import { log } from "console";
 
 const BookCart = () => {
   const cart = useAppSelector(getCart);
   const dispatch = useAppDispatch();
-
+   console.log(cart);
+   
   return (
     <div className="bg-gray-100 min-h-screen   p-4">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden  mx-auto">
@@ -20,7 +22,7 @@ const BookCart = () => {
             <h2 className="text-2xl font-bold mb-4 text-center">
               Shopping Cart
             </h2>
-            {cart.map((product: any,index:number) => (
+            {cart.map((product: any, index: number) => (
               <div
                 key={product.id}
                 className={`flex justify-between flex-wrap p-4 ${
@@ -28,50 +30,50 @@ const BookCart = () => {
                 }`}
               >
                 <div className="flex justify-evenly">
-                <div className="w-1/6 p-2">
-                  <Image
-                    src={product.image}
-                    alt={product.title}
-                    width={100}
-                    height={100}
-                    className="object-contain"
-                  />
-                </div>
-                <div className=" p-2">
-                  <h2 className="text-lg font-semibold">{product.title}</h2>
-                  <p className="text-lg font-semibold text-green-500">
-                    In Stock
-                  </p>
-                  <p className="text-lg font-semibold">
-                    Price: ${product.price.toFixed(2)}
-                  </p>
-                  <div className="flex items-center">
-                    <label className="mr-2">Quantity:</label>
-                    <button
-                      // onClick={() => handleQuantityChange(product.id, product.quantity - 1)}
-                      className="bg-gray-300 px-2 py-1 rounded-l"
-                    >
-                      -
-                    </button>
-                    <input
-                      type="number"
-                      value={product.quantity}
-                      // onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value))}
-                      className="w-12 p-1 border-t border-b text-center"
-                      min="1"
+                  <div className="w-1/6 p-2">
+                    <Image
+                      src={product.image}
+                      alt={product.title}
+                      width={100}
+                      height={100}
+                      className="object-contain"
                     />
-                    <button
-                      // onClick={() => handleQuantityChange(product.id, product.quantity + 1)}
-                      className="bg-gray-300 px-2 py-1 rounded-r"
-                    >
-                      +
-                    </button>
                   </div>
-                </div>
+                  <div className=" p-2">
+                    <h2 className="text-lg font-semibold">{product.title}</h2>
+                    <p className="text-lg font-semibold text-green-500">
+                      In Stock
+                    </p>
+                    <p className="text-lg font-semibold">
+                      Price: ${product.price.toFixed(2)}
+                    </p>
+                    <div className="flex items-center">
+                      <label className="mr-2">Quantity:</label>
+                      <button
+                        // onClick={() => handleQuantityChange(product.id, product.quantity - 1)}
+                        className="bg-gray-300 px-2 py-1 rounded-l"
+                      >
+                        -
+                      </button>
+                      <input
+                        type="number"
+                        value={product.quantity}
+                        // onChange={(e) => handleQuantityChange(product.id, parseInt(e.target.value))}
+                        className="w-12 p-1 border-t border-b text-center"
+                        min="1"
+                      />
+                      <button
+                        // onClick={() => handleQuantityChange(product.id, product.quantity + 1)}
+                        className="bg-gray-300 px-2 py-1 rounded-r"
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
                 </div>
                 <div className="w-1/6 p-2 text-right">
                   <button
-                    // onClick={() => handleRemove(product.id)}
+                    onClick={() => dispatch(removeFromCart(product.id))}
                     className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
                   >
                     Remove
--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -2,25 +2,40 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 
 interface CartState {
-    cart:any
+  cart: any;
 }
 
-const initialState:CartState={
-    cart:[]
-}
+const initialState: CartState = {
+  cart: [],
+};
 
-const cartSlice =createSlice({
-    name:"cart",
-    initialState,
-    reducers:{
-        addToCart:(state,action)=>{
-            state.cart.push(action.payload)
-        }
-    }
-})
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const alredyExist = state.cart.find(
+        (item: any) =>  item.id === action.payload.id
+      );
+      if (alredyExist) {
+        state.cart = state.cart.map((item: any) => {
+          return item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item;
+        });
+      } else {
+        state.cart.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((item: any) => {
+        return item.id !== action.payload;
+      });
+    },
+  },
+});
 
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
-export const {addToCart}=cartSlice.actions;
-
-export const getCart = (state: RootState) => state.cart.cart
+export const getCart = (state: RootState) => state.cart.cart;
 export default cartSlice.reducer;
